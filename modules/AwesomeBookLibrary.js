4cc25ad33e4cc0e481c41314bfcf28754866a2cf
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-05 20:46:10 -0400

    Create the AwesomeBookLibrary.js module

---
+++ modules/AwesomeBookLibrary.js
@@ -0,0 +1,9 @@
+import Book from './Book.js';
+
+class AwesomeBookLibrary {
+  constructor() {
+    this.books = [];
+    this.booksContainer = document.querySelector('#books-container');
+    this.formAddBook = document.querySelector('#form-book');
+  }
+}
