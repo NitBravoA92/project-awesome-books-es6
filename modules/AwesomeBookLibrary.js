89d3ab49d940ea2434c325b07bd185b366def09e
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-05 21:00:41 -0400

    Update the generateBookItem method

--- modules/AwesomeBookLibrary.js
+++ modules/AwesomeBookLibrary.js
@@ -1,28 +1,28 @@
-import Book from './Book.js';
+import Book from "./Book.js";
 
 class AwesomeBookLibrary {
   constructor() {
     this.books = [];
-    this.booksContainer = document.querySelector('#books-container');
-    this.formAddBook = document.querySelector('#form-book');
+    this.booksContainer = document.querySelector("#books-container");
+    this.formAddBook = document.querySelector("#form-book");
   }
 
   initBooks() {
-    if (localStorage.getItem('library')) {
-      this.books = [...JSON.parse(localStorage.getItem('library'))];
+    if (localStorage.getItem("library")) {
+      this.books = [...JSON.parse(localStorage.getItem("library"))];
       this.renderBooks();
     }
   }
-  
+
   renderBooks() {
-    this.booksContainer.innerHTML = '';
+    this.booksContainer.innerHTML = "";
     this.books.forEach((book, index) => {
       this.booksContainer.appendChild(this.generateBookItem(book, index));
     });
   }
 
   storageBooks() {
-    localStorage.setItem('library', JSON.stringify(this.books));
+    localStorage.setItem("library", JSON.stringify(this.books));
   }
 
   removeBook = (event) => {
@@ -30,29 +30,30 @@ class AwesomeBookLibrary {
     this.books.splice(bookIndex, 1);
     this.storageBooks();
     this.renderBooks();
-  }
+  };
 
   generateBookItem(book, index) {
-    const bookItem = document.createElement('div');
-    const bookDetails = document.createElement('div');
-    const bookTitle = document.createElement('h3');
-    const bookAuthor = document.createElement('p');
-    const bookBtnRemove = document.createElement('button');
-    bookItem.classList.add('book');
+    const bookItem = document.createElement("div");
+    const bookDetails = document.createElement("div");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const bookBtnRemove = document.createElement("button");
+    bookItem.classList.add("book");
     bookItem.dataset.item = index;
-    bookDetails.classList.add('book-details');
-    bookTitle.classList.add('show-title');
+    bookDetails.classList.add("book-details");
+    bookTitle.classList.add("show-title");
     bookTitle.textContent = book.title;
-    bookAuthor.classList.add('show-author');
+    bookAuthor.classList.add("show-author");
     bookAuthor.innerHTML = `by <span class="show-author">${book.author}</span>`;
-    bookBtnRemove.classList.add('removeBook');
-    bookBtnRemove.setAttribute('type', 'button');
-    bookBtnRemove.textContent = 'Remove';
+    bookBtnRemove.classList.add("removeBook");
+    bookBtnRemove.setAttribute("type", "button");
+    bookBtnRemove.textContent = "Remove";
     bookDetails.appendChild(bookTitle);
     bookDetails.appendChild(bookAuthor);
     bookItem.appendChild(bookDetails);
     bookItem.appendChild(bookBtnRemove);
+    /* Handle the click event on the 'Remove' button of each Book item */
+    bookBtnRemove.addEventListener("click", this.removeBook);
     return bookItem;
   }
-
 }
