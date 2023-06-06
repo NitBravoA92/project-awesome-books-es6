af504ba1d9722f6bcaca952a6c0b16988138ca69
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-05 20:47:50 -0400

    Create the generateBookItem method in AwesomeBookLibrary class

--- modules/AwesomeBookLibrary.js
+++ modules/AwesomeBookLibrary.js
@@ -10,5 +10,28 @@ class AwesomeBookLibrary {
   storageBooks() {
     localStorage.setItem('library', JSON.stringify(this.books));
   }
-  
+
+  generateBookItem(book, index) {
+    const bookItem = document.createElement('div');
+    const bookDetails = document.createElement('div');
+    const bookTitle = document.createElement('h3');
+    const bookAuthor = document.createElement('p');
+    const bookBtnRemove = document.createElement('button');
+    bookItem.classList.add('book');
+    bookItem.dataset.item = index;
+    bookDetails.classList.add('book-details');
+    bookTitle.classList.add('show-title');
+    bookTitle.textContent = book.title;
+    bookAuthor.classList.add('show-author');
+    bookAuthor.innerHTML = `by <span class="show-author">${book.author}</span>`;
+    bookBtnRemove.classList.add('removeBook');
+    bookBtnRemove.setAttribute('type', 'button');
+    bookBtnRemove.textContent = 'Remove';
+    bookDetails.appendChild(bookTitle);
+    bookDetails.appendChild(bookAuthor);
+    bookItem.appendChild(bookDetails);
+    bookItem.appendChild(bookBtnRemove);
+    return bookItem;
+  }
+
 }
