6893fe2aa5239feebeb740f5e7800d748768ba7e
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-05 21:47:09 -0400

    Create and Export the PageNavigation for the navigation of the webpage

--- modules/Header.js
+++ modules/Header.js
@@ -1,5 +1,29 @@
 import { DateTime } from './luxon.js';
 
+export const PageNavigation = () => {
+  const pagesLinks = document.querySelectorAll('.nav-link');
+  let pageId;
+  let pageContainer;
+  pagesLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      for (let index = 0; index < pagesLinks.length; index += 1) {
+        pageId = pagesLinks[index].dataset.url;
+        pageContainer = document.querySelector(`#${pageId}`);
+        if (!pageContainer.classList.contains('hide')) {
+          pageContainer.classList.add('hide');
+          pagesLinks[index].classList.remove('change-color');
+          break;
+        }
+      }
+      link.classList.add('change-color');
+      pageId = link.dataset.url;
+      pageContainer = document.querySelector(`#${pageId}`);
+      pageContainer.classList.remove('hide');
+    });
+  });
+};
+
 export const currentDateTime = () => {
   const dateContainer = document.querySelector('#date-container');
   setInterval(() => {
