ecb710186d02ee6a24cb60210710be3a939bf17e
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-05 21:31:44 -0400

    Create and Export the currentDateTime function to display the current DateTime

--- modules/Header.js
+++ modules/Header.js
@@ -1,1 +1,14 @@
 import { DateTime } from './luxon.js';
+
+export const currentDateTime = () => {
+  const dateContainer = document.querySelector('#date-container');
+  setInterval(() => {
+    const dateNow = DateTime.now();
+    dateContainer.textContent = dateNow.toLocaleString(
+      {
+        ...DateTime.DATE_FULL,
+        ...DateTime.TIME_WITH_SECONDS,
+      },
+    );
+  }, 1000);
+};
