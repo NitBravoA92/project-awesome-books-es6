0b2b9ac824af0a3dc29ce132a1a6c67ce2782664
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-05 21:32:56 -0400

    Import currentDateTime function from Header.js module

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import AwesomeBookLibrary from './modules/AwesomeBookLibrary.js';
+import { currentDateTime } from './modules/Header.js';
+
+currentDateTime();
 
 const app = new AwesomeBookLibrary();
 app.initBooks();
